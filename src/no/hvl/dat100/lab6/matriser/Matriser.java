4caf033aa31ce11fca8c8728a3491c59352a278e
chris <chris@LAPTOP-3B307CQ9.home>
2022-09-24 20:37:51 +0200

    O1-B (e/f)

--- src/no/hvl/dat100/lab6/matriser/Matriser.java
+++ src/no/hvl/dat100/lab6/matriser/Matriser.java
@@ -35,16 +35,37 @@ public class Matriser {
 	// e)
 	public static int[][] speile(int[][] matrise) {
 
-		// TODO
-		throw new UnsupportedOperationException("speile ikke implementert");
+		int[][] newM = new int[matrise.length][matrise[0].length];
+		for (int i = 0; i < matrise.length; i++)
+			for (int j = 0; j < matrise.length; j++) {
+				newM[i][j] = matrise[j][i];
+			}
+		return newM;
+		// throw new UnsupportedOperationException("speile ikke implementert");
 	
 	}
 
 	// f)
 	public static int[][] multipliser(int[][] a, int[][] b) {
 
-		// TODO
-		throw new UnsupportedOperationException("multipliser ikke implementert");
+		int[][] nyB = new int[b[0].length][b.length];
+		for (int x = 0; x < b.length; x++)
+			for (int y = 0; y < b[0].length; y++)
+				nyB[y][x] = b[x][y];
+
+		int[][] newM = new int[a.length][b.length];
+		
+		
+		
+		for (int x = 0; x < newM.length; x++)
+			for (int y = 0; y < newM[0].length; y++) {
+				int sum =0;
+				for (int x2=0;x2<newM.length;x2++)
+					sum=sum+a[x][x2]*nyB[y][x2];
+				newM[x][y] =sum;
+			}
+		return newM;
+		// throw new UnsupportedOperationException("multipliser ikke implementert");
 	
 	}
 }
